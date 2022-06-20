7a65562e8f6c2f013b037984a55a5d46b51458bd
Gia Bao <giabao.nguyenhoang1001@gmail.com>
2022-06-20 15:39:17 +0700

    Managing State Better

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,29 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-import TodoList from './components/TodoList';
-import NewTodo from './components/NewTodo';
-import { Todo } from './todo.model';
+import TodoList from "./components/TodoList";
+import NewTodo from "./components/NewTodo";
+import { Todo } from "./todo.model";
 
 // const todos = [
-  //   {id: 't1', text: 'Finish the course'},
-  //   {id: 't2', text: 'Finish Web Dev Bootcamp'}
-  // ];
-  //Should not return void type for App component
-  function App() {
-    const [todos, setTodos] = useState<Todo[]>([]);
+//   {id: 't1', text: 'Finish the course'},
+//   {id: 't2', text: 'Finish Web Dev Bootcamp'}
+// ];
+//Should not return void type for App component
+function App() {
+  const [todos, setTodos] = useState<Todo[]>([]);
 
-    const todoAddHandler = (text: string) => {
-      setTodos([{id: Math.random().toString(), text: text}]);
-    };
-    
-    return (
-      <div className="App">
-      <NewTodo onAddTodo={todoAddHandler}/>
+  const todoAddHandler = (text: string) => {
+    setTodos((prevTodos) => [
+      ...prevTodos,
+      { id: Math.random().toString(), text: text },
+    ]);
+  };
+
+  return (
+    <div className="App">
+      <NewTodo onAddTodo={todoAddHandler} />
       {/* A component that adds todos */}
-      <TodoList items={todos}/>
+      <TodoList items={todos} />
     </div>
   );
 }
