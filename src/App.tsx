a525cb00646194a8f34162d23777f0a449dbd008
Gia Bao <giabao.nguyenhoang1001@gmail.com>
2022-06-20 15:35:18 +0700

    Working with State & Types

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import TodoList from './components/TodoList';
 import NewTodo from './components/NewTodo';
+import { Todo } from './todo.model';
 
-const todos = [
-  {id: 't1', text: 'Finish the course'},
-  {id: 't2', text: 'Finish Web Dev Bootcamp'}
-];
-//Should not return void type for App component
-function App() {
-  const todoAddHandler = (text: string) => {
-    console.log(text);
-  };
+// const todos = [
+  //   {id: 't1', text: 'Finish the course'},
+  //   {id: 't2', text: 'Finish Web Dev Bootcamp'}
+  // ];
+  //Should not return void type for App component
+  function App() {
+    const [todos, setTodos] = useState<Todo[]>([]);
 
-  return (
-    <div className="App">
+    const todoAddHandler = (text: string) => {
+      setTodos([{id: Math.random().toString(), text: text}]);
+    };
+    
+    return (
+      <div className="App">
       <NewTodo onAddTodo={todoAddHandler}/>
       {/* A component that adds todos */}
       <TodoList items={todos}/>
