ed4ebd47494cc2567ef38f71011997bff3d5413d
Gia Bao <giabao.nguyenhoang1001@gmail.com>
2022-06-20 15:20:57 +0700

    get user input with refs

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
 import React from 'react';
 
 import TodoList from './components/TodoList';
+import NewTodo from './components/NewTodo';
+
+const todos = [
+  {id: 't1', text: 'Finish the course'},
+  {id: 't2', text: 'Finish Web Dev Bootcamp'}
+];
 //Should not return void type for App component
 function App() {
-  const todos = [
-    {id: 't1', text: 'Finish the course'},
-    {id: 't2', text: 'Finish Web Dev Bootcamp'}
-];
   return (
     <div className="App">
+      <NewTodo />
       {/* A component that adds todos */}
       <TodoList items={todos}/>
     </div>
